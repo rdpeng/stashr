017d1f8f34c3b3f9f2b991ff20e34b3b50acbc32
rpeng <rpeng>
2006-06-23 18:17:09 +0000

    fiddling

--- R/filehash-Remote.R
+++ R/filehash-Remote.R
@@ -1,3 +1,34 @@
 setClass("filehashRemote")
 
+createRemote <- function(dbName) {
 
+}
+
+initializeRemote <- function(dbName) {
+
+}
+
+setMethod("dbInsert", signature(db = "filehashRemote", key = "character", value = "ANY"),
+          function(db, key, value) {
+              stop("cannot insert into a 'filehashRemote' database")
+          })
+
+setMethod("dbFetch", signature(db = "filehashRemote", key = "character"),
+          function(db, key) {
+
+          })
+              
+setMethod("dbDelete", signature(db = "filehashRemote", key = "character"),
+          function(db, key) {
+              stop("cannot delete from a 'filehashRemote' database")
+          })
+
+setMethod("dbList", "filehashRemote",
+          function(db) {
+
+          })
+
+setMethod("dbExists", signature(db = "filehashRemote", key = "character"),
+          function(db, key) {
+
+          })
