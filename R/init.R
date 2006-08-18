ba971f34c09a0600fe31c664072e54a9aeaae4db
rpeng <rpeng>
2006-08-18 19:32:03 +0000

    initial stab at init functions

--- R/init.R
+++ R/init.R
@@ -1,8 +1,12 @@
 createRemote <- function(dbName) {
-
+    if(!all(c("url", "dir") %in% names(dbName)))
+        stop("'dbName' should be a list with elements 'url' and 'dir'")
+    with(dbName, create(url, dir))
+    TRUE
 }
 
 initializeRemote <- function(dbName) {
-
+    r <- init(dbName)
+    new("filehashRemote", url = r$url, dir = r$dir)
 }
 
