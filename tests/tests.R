5befcf164fc686c0e5d36be211aeb7fbf44cae67
rpeng <rpeng>
2006-09-05 13:08:12 +0000

    wrap error-producing tests in a 'try'

--- tests/tests.R
+++ tests/tests.R
@@ -1,6 +1,8 @@
 ## Tests for Regression testing ##########################################
 ##########################################################################
 
+library(filehashRemote)
+
 myurl <- "http://www.biostat.jhsph.edu/MCAPS/data/"
 wd <- getwd()
 dir <- file.path(wd,"testDir")
@@ -14,18 +16,18 @@ show(db@dir)
 
 
 ## other prelim steps necessary ##
-create(db@url,db@dir)
+dbCreate(c(url = db@url, dir = db@dir))
 
 ## test the methods ##
 dbList(db)
 dbList(db, save = TRUE)
 dbFetch(db, "01003") 
 dbFetch(db, "01003", offline = TRUE) 
-dbFetch(db, "01004")
-dbFetch(db, "01004", offline = TRUE)
-dbDelete(db,"01003")
-dbInsert(db,key = "01004", value = NULL)
+try( dbFetch(db, "01004") )
+try( dbFetch(db, "01004", offline = TRUE) )
+try( dbDelete(db,"01003") )
+try( dbInsert(db,key = "01004", value = NULL) )
 dbSync(db, key = NULL)
 dbSync(db, key = c("01003"))
-dbSync(db, key = c("01004","01003"))
+try( dbSync(db, key = c("01004","01003")) )
 dbExists(db,c("01003", "01004","55079"))
