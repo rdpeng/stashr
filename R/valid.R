19c58a97e0c91e1ceae344e0da7ef097ff380509
rpeng <rpeng>
2006-09-12 13:25:49 +0000

    add validity method for 'filehashLocal'

--- R/valid.R
+++ R/valid.R
@@ -2,5 +2,16 @@ setValidity("filehashRemote",
             function(object) {
                 if(length(object@name) == 0)
                     return("'filehashRemote' object needs a 'name'")
+                if(length(grep("^http://", object@url, fixed = TRUE, perl = TRUE)) == 0)
+                    return("'filehashRemote' object needs a 'url' of type 'http://'")
+                TRUE
+            })
+
+setValidity("filehashLocal",
+            function(object) {
+                if(length(object@name) == 0)
+                    return("'filehashLocal' object needs a 'name'")
+                if(length(grep("^file://", object@url, fixed = TRUE, perl = TRUE)) == 0)
+                    return("'filehashLocal' object needs a 'url' of type 'file://'")
                 TRUE
             })
