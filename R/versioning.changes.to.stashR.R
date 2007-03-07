0be278f740c81cc06b0d4a909557de3cda2f2ce7
seckel <seckel>
2007-03-07 03:45:05 +0000

    initial changes to functions for remoteDB ... note I didn't use .SIG files in dbFetch method for remoteDB objects for now

--- R/versioning.changes.to.stashR.R
+++ R/versioning.changes.to.stashR.R
@@ -54,7 +54,7 @@ updatedReposVersionInfo <- function(db, key, keepKey = TRUE){
 	if(length(info)!=0){
 		keyFiles <- strsplit(info[length(info)], ":")[[1]][2]
 		keyFilesSep <- strsplit(keyFiles," ")[[1]]
-		v <- grep(paste("^",key,"\\.[1-9]+$",sep=""),keyFilesSep)
+		v <- grep(paste("^",key,"\\.[0-9]+$",sep=""),keyFilesSep)
 
 		if(length(v)==0)
                     others <- keyFilesSep
@@ -167,7 +167,7 @@ setMethod("dbList", "localDB",
 		if(length(info)!=0){
 			keyFiles <- strsplit(info[length(info)], ":")[[1]][2]
 			keyFilesSep <- strsplit(keyFiles," ")[[1]]
-			gsub("\\.[1-9]+$", "", keyFilesSep)
+			gsub("\\.[0-9]+$", "", keyFilesSep)
 		}
           })
 
@@ -189,5 +189,79 @@ setMethod("dbDelete", signature(db = "localDB", key = "character"),
 ## remote DB ###############################
 ############################################
 
+## note dbInsert stays the same (doesn't work for remoteDB) ##
 
-	
+## currently ignoring dbSync and the associated utility functions
+## readRemoteSIG, readLocalSIG and checkSIG
+
+
+####################
+## getdata ######### downloads the key & the SIG file
+####################
+
+getdata <- function(db,key){
+    localFiles <- c(data = local.file.path(db, key, objectVersion(db,key)),
+                    sig = local.file.path.SIG(db, key, objectVersion(db,key))
+
+    handler <- function(cond) {
+        ## If a condition is thrown (e.g. error or interrupt), delete
+        ## whatever was downloaded
+        ex <- file.exists(localFiles)
+        file.remove(localFiles[ex])
+        cond
+    }
+    status <- tryCatch({
+        download.file(file.path(db@url, "data", key, objectVersion(db,key)),
+                      localFiles["data"], mode = "wb", cacheOK = FALSE,
+                      quiet = .stashROptions$quietDownload)
+        download.file(file.path(db@url, "data", paste(key, objectVersion(db,key), ".SIG", sep = "")),
+                      localFiles["sig"], mode = "wb", cacheOK = FALSE,
+                      quiet = .stashROptions$quietDownload)
+    }, error = handler, interrupt = handler)
+
+    if(inherits(status, "condition"))
+        stop(gettextf("problem downloading data for key '%s': %s",
+                      key, conditionMessage(status)))
+}
+
+
+setMethod("dbFetch", signature(db = "remoteDB", key = "character"),
+          function(db, key, offline = FALSE, ...){
+              if(offline && !checkLocal(db,key))
+                  stop("have not previously downloaded specified data ", 
+                       "and 'offline = TRUE'") 
+              if(!offline && !(key %in% dbList(db)))
+                  stop("specified key not in database")
+		  # ignoring .SIG files for now ?	
+              #if(!offline && checkLocal(db, key)) {
+              #    ## Check the remote/local MD5 hash value
+              #    if(!checkSIG(db, key))
+              #        getdata(db,key)
+              #}
+              if(!checkLocal(db, key))
+                  getdata(db,key)
+              read(db, key)
+          })
+
+
+setMethod("dbDelete", signature(db = "remoteDB", key = "character"),
+          function(db, key, ...) {
+              stop("cannot delete from a 'remoteDB' database")
+          })
+
+
+setMethod("dbList", "remoteDB",
+          function(db, ...){
+		info <- reposVersionInfo(db)
+		if(length(info)!=0){
+			keyFiles <- strsplit(info[length(info)], ":")[[1]][2]
+			keyFilesSep <- strsplit(keyFiles," ")[[1]]
+			gsub("\\.[0-9]+$", "", keyFilesSep)
+		}
+          })
+
+
+setMethod("dbExists", signature(db = "remoteDB", key = "character"),
+          function(db, key, ...){
+              key %in% dbList(db)  ## returns a vector of TRUE/FALSE
+          })
