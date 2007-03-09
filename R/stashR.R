b3d2937a301b6ee2d4a63714fc80c0016b5c6011
seckel <seckel>
2007-03-09 13:35:07 +0000

    updated the rest of the file with changes from the 'versioning changes to stashR' file

--- R/stashR.R
+++ R/stashR.R
@@ -234,24 +234,145 @@ setMethod("dbSync", signature(db = "remoteDB"),
 
 
 ########################################################################
-## Prelim Functions  ###################################################
+## Utility Functions  ##################################################
 ########################################################################
+
+
+## use readLines to find last line, returns as character string
+reposVersionInfo <- function(db){
+	if(class(db)=="localDB")
+		verDir <- db@dir
+	else  verDir <- db@url
+	if({class(db)=="localDB" & file.exists(file.path(verDir, "version"))}
+		| class(db)=="remoteDB"){  
+		con <- file(file.path(verDir, "version"))
+		open(con, "r")  ## 'version' is a text file
+		on.exit(close(con))
+		VerList <- readLines(con)
+		ifelse(db@reposVersion == - 1, 
+			rvn <- length(VerList), 
+			rvn <- db@reposVersion)
+		VerList[rvn]
+	}
+	else character(0)	
+}
+
+## returns "object version" associated with a given key
+
+objectVersion <- function(db, key){
+	## for localDB: 
+	## determine last version of the object in the repository    ##
+	## (note this object may have been previously deleted, so    ##
+	## we need to look in the data directory at the data files)  ##
+	## for remoteDB:
+	## read pertinent line of the version file from the internet ##
+	if(class(db)=="localDB"){
+		allFiles <- list.files(file.path(db@dir,"data"))
+		keyFiles <- allFiles[-grep("\\.SIG$",allFiles)]	## returns character(0) if no files
+		o <- order(keyFiles[grep(paste("^",key,"\\.[0-9]+$",sep=""),keyFiles)],decreasing=FALSE)
+		oFiles <- keyFiles[o]
+		latestFile <-oFiles[length(oFiles)]
+		if(length(latestFile)!=0){
+			latestFileSplit <- strsplit(latestFile,"\\.")[[1]]
+			lastObjVer <- as.numeric(latestFileSplit[length(latestFileSplit)])
+		}
+		else lastObjVer <- 0
+		lastObjVer
+	}
+	else{ 
+		info <- reposVersionInfo(db)
+		if(length(info)!=0){
+			keyFiles <- strsplit(info[length(info)], ":")[[1]][2]
+			keyFilesSep <- strsplit(keyFiles," ")[[1]]
+			v <- grep(paste("^",key,"\\.[0-9]+$",sep=""),keyFilesSep,value=TRUE)
+			currNum <- as.numeric(substring(v,nchar(key)+2))
+			if(length(currNum)==0) currNum <- 0
+			currNum
+		}
+		else 0
+	}
+}
+
+
+
+
+## function returning the integer corresponding to the lastest repos
+## version by reading first integer of the last line of the version
+## file
+
+latestReposVersionNum <- function(db){ 
+	info <- reposVersionInfo(db)
+	if(length(info)!=0){
+		as.numeric(strsplit(info[length(info)], ":")[[1]][1])
+	}
+	else 0
+}
+
+
+## reads last line of the version file creates new info on repository
+## version to be inserted in last line of the version file updates
+## keepKey = FALSE deletes the key from the version information
+
+
+updatedReposVersionInfo <- function(db, key, keepKey = TRUE){
+	## find and remove key (for updating) from latest repository version info ##
+	reposV <- latestReposVersionNum(db)+1
+	info <- reposVersionInfo(db)
+	if(length(info)!=0){
+		keyFiles <- strsplit(info[length(info)], ":")[[1]][2]
+		keyFilesSep <- strsplit(keyFiles," ")[[1]]
+		v <- grep(paste("^",key,"\\.[0-9]+$",sep=""),keyFilesSep)
+
+		if(length(v)==0)
+                    others <- keyFilesSep
+                else
+                    others <- keyFilesSep[-v]
+		if(length(others)==0)
+			othersCollapsed <- paste(others, collapse=" ")
+		else{	
+			if(length(others)==1 & is.na(others[1]))
+				othersCollapsed <- character(0)
+			else 
+				othersCollapsed <- paste(others, collapse=" ")
+		}
+		newKeyInfo <- paste(key,objectVersion(db,key)+1, sep=".")
+		if(keepKey)
+			updatedKeyFiles <- paste(othersCollapsed, newKeyInfo, sep=" ")
+		else
+			updatedKeyFiles <- othersCollapsed
+	}
+	else updatedKeyFiles <- paste(key,1,sep=".")
+	# remove leading space resulting from intially inserting same key twice#
+	updatedKeyFiles <- gsub("^ ","",updatedKeyFiles)
+	paste(reposV, updatedKeyFiles, sep = ":") 
+}
+
+
+
+######### updating version file ########## 
+updateVersion <- function(db,key, keepKey = TRUE){
+	cat(updatedReposVersionInfo(db,key,keepKey),
+            file = file.path(db@dir,"version"),
+            sep = "\n", append = TRUE)
+}
+
 
 ###############################
 ## local.file.path ############  Creates a file path in the local data  
 ###############################  directory (to be used internally).	
 
-local.file.path <- function(db,key){
-    file.path(path.expand(db@dir), "data", key)
+local.file.path <- function(db,key,objVerNum=objectVersion(db, key)){
+    	file.path(path.expand(db@dir), "data", paste(key,".",objVerNum,sep=""))
 }
 
 ###############################
 ## local.file.path.SIG ######## Creates a file path in the local data  
 ############################### directory (to be used internally) for the SIG files.	
 
-local.file.path.SIG <- function(db,key){
-    file.path(path.expand(db@dir), "data", paste(key,".SIG",sep=""))
+local.file.path.SIG <- function(db,key,objVerNum=objectVersion(db, key)){
+	file.path(path.expand(db@dir), "data", paste(key,".",objVerNum,".SIG",sep=""))
 }
+
 
 
 #################### Returns TRUE if data file for 'key' is in local dir, otherwise
@@ -259,12 +380,12 @@ local.file.path.SIG <- function(db,key){
 #################### with more than one key.
 
 checkLocal <- function(db, key){
-    ## key %in% list.files(file.path(db@dir, "data"), all.files = TRUE)
-    datadir <- file.path(db@dir, "data")
-
-    if(!file.exists(datadir))
+    	## key %in% list.files(file.path(db@dir, "data"), all.files = TRUE)
+    	datadir <- file.path(db@dir, "data")
+	
+	if(!file.exists(datadir))
         stop("local data directory does not exist")
-    file.exists(file.path(datadir, key))      ## returns a vector of T/F
+    	file.exists(local.file.path(db,key))      ## returns a vector of T/F
 }
 
 
@@ -305,15 +426,14 @@ getdata <- function(db,key){
 ## read ############ Reads file associated with specified key from the local directory.
 #################### Returns the data object associated with the key.
 
+
 read <- function(db, key){
     if(!checkLocal(db,key))
-        stop(gettextf("files associated with key '%s' not yet downloaded", key))
+        stop(gettextf("files associated with key not yet downloaded", key))
     con <- gzfile(local.file.path(db,key))
     open(con, "rb")
     on.exit(close(con))
     unserialize(con) 
 }
-
-
 
 
