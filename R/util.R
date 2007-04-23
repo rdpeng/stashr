2af2265e3fe70d7ca464296e2bbcc4f1e85c042c
Roger D. Peng [audrey] <rpeng@jhsph.edu>
2007-04-23 15:23:14 -0400

    Indenting; tweak error message

--- R/util.R
+++ R/util.R
@@ -1,34 +1,34 @@
 setGeneric("setDir<-", function(db, value) standardGeneric("setDir<-"))
 
 setReplaceMethod("setDir", signature(db = "remoteDB", value = "character"),
-          function(db, value) {
-              db@dir <- value
-              db
-          })
+                 function(db, value) {
+                         db@dir <- value
+                         db
+                 })
 
 setGeneric("copyDB", function(db, ...) standardGeneric("copyDB"))
 
 setMethod("copyDB", "localDB",
           function(db, dir, ...) {
-              if(identical(dir, db@dir))
-                  stop("cannot copy 'localDB' object to original directory")
-              newdb <- new("localDB", dir = dir, name = db@name)
-              keys <- dbList(db)
+                  if(identical(dir, db@dir))
+                          stop("cannot copy 'localDB' to original directory")
+                  newdb <- new("localDB", dir = dir, name = db@name)
+                  keys <- dbList(db)
 
-              for(key in keys) 
-                  dbInsert(newdb, key, dbFetch(db, key))
-              newdb
+                  for(key in keys) 
+                          dbInsert(newdb, key, dbFetch(db, key))
+                  newdb
           })
 
 getCurrentReposVersion <- function(db) {
-    info <- reposVersionInfo(db)
+        info <- reposVersionInfo(db)
 
-    if(length(info) > 0) {
-        num <- strsplit(info, ":", fixed = TRUE)[[1]][1]
-        as.numeric(num)
-    }
-    else
-        0
+        if(length(info) > 0) {
+                num <- strsplit(info, ":", fixed = TRUE)[[1]][1]
+                as.numeric(num)
+        }
+        else
+                0
 }
 
 setGeneric("reposVersion",
@@ -36,12 +36,12 @@ setGeneric("reposVersion",
 
 setMethod("reposVersion", "localDB",
           function(db, ...) {
-              getCurrentReposVersion(db)
+                  getCurrentReposVersion(db)
           })
 
 setMethod("reposVersion", "remoteDB",
           function(db, ...) {
-              getCurrentReposVersion(db)
+                  getCurrentReposVersion(db)
           })
 
 setGeneric("reposVersion<-",
@@ -50,30 +50,39 @@ setGeneric("reposVersion<-",
 setReplaceMethod("reposVersion",
                  signature(db = "remoteDB", value = "numeric"),
                  function(db, value) {
-                     db@reposVersion <- value
-                     db
+                         db@reposVersion <- value
+                         db
                  })
 
 setReplaceMethod("reposVersion",
                  signature(db = "localDB", value = "numeric"),
                  function(db, value) {
-                     db@reposVersion <- value
-                     db
+                         db@reposVersion <- value
+                         db
                  })
 
 convertOldStashR <- function() {
-    infiles <- dir(".", all.files = TRUE)
-    use <- !file.info(infiles)$isdir
-    infiles <- infiles[use]
-    outfiles <- sub("\\.SIG$", ".1.SIG", infiles)
-    sigfiles <- grep("\\.SIG$", outfiles)
-    outfiles[-sigfiles] <- paste(outfiles[-sigfiles], "1", sep = ".")
+        infiles <- dir(".", all.files = TRUE)
+        use <- !file.info(infiles)$isdir
+        infiles <- infiles[use]
+        outfiles <- sub("\\.SIG$", ".1.SIG", infiles)
+        sigfiles <- grep("\\.SIG$", outfiles)
+        outfiles[-sigfiles] <- paste(outfiles[-sigfiles], "1", sep = ".")
 
-    for(i in seq(along = infiles)) {
-        file.rename(infiles[i], outfiles[i])
-    }
-    keys <- infiles[-sigfiles]
-    vline <- paste("1", paste(keys, "1", sep = ".", collapse = " "), sep = ":")
-    writeLines(vline, "../version")
+        for(i in seq(along = infiles)) {
+                file.rename(infiles[i], outfiles[i])
+        }
+        keys <- infiles[-sigfiles]
+        vline <- paste("1", paste(keys, "1", sep = ".", collapse = " "), sep = ":")
+        writeLines(vline, "../version")
 
+}
+
+
+################################################################################
+## Check validity of the data using MD5 digests
+################################################################################
+
+validData <- function(db, key) {
+        
 }
