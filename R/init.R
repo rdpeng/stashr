cb75ce82a094f651b535257bab9efa9752023d5c
Roger D. Peng [audrey] <rpeng@jhsph.edu>
2007-07-03 14:45:18 -0400

    Indenting

--- R/init.R
+++ R/init.R
@@ -3,59 +3,61 @@
 
 setMethod("initialize", "remoteDB",
           function(.Object, ...) {
-              .Object <- callNextMethod()
-              .Object <- dbCreate(.Object)
-              cacheVersionFile(.Object)
-              .Object
+                  .Object <- callNextMethod()
+                  .Object <- dbCreate(.Object)
+                  cacheVersionFile(.Object)
+                  .Object
           })
 
 setMethod("initialize", "localDB",
           function(.Object, ...) {
-              .Object <- callNextMethod()
-              .Object <- dbCreate(.Object)
-              .Object
+                  .Object <- callNextMethod()
+                  .Object <- dbCreate(.Object)
+                  .Object
           })
 
 createLocalDir <- function(db) {
-    ## create the local main directory and data sub-directory to
-    ## store the data files ##
-    datadir <- file.path(db@dir,"data")
-    status <- dir.create(db@dir, showWarnings = FALSE, recursive = TRUE)
-    
-    if(!status && !file.exists(db@dir))
-        stop(gettextf("problem creating directory '%s'", db@dir))
-    status <- dir.create(datadir, showWarnings = FALSE, recursive = TRUE)
-    
-    if(!status && !file.exists(datadir))
-        stop(gettextf("problem creating directory '%s'", datadir))
+        ## create the local main directory and data sub-directory to
+        ## store the data files ##
+        datadir <- file.path(db@dir,"data")
+        status <- dir.create(db@dir, showWarnings = FALSE,
+                             recursive = TRUE)
+        
+        if(!status && !file.exists(db@dir))
+                stop(gettextf("problem creating directory '%s'", db@dir))
+        status <- dir.create(datadir, showWarnings = FALSE,
+                             recursive = TRUE)
+        
+        if(!status && !file.exists(datadir))
+                stop(gettextf("problem creating directory '%s'", datadir))
 }
 
 setMethod("dbCreate",
           signature(db = "remoteDB"),
           function(db, ...) {
-              ## remove trailing "/" on dir and url ##
-              db@dir <- sub("\\/$","", db@dir, perl = TRUE)
-              db@url <- sub("\\/$","", db@url, perl = TRUE)
-              
-              createLocalDir(db)
- 
-              ## save url in the R workspace format in the main directory ##
-              myurl <- db@url 
-              save(myurl, file = file.path(db@dir, "url"))
-              invisible(db)
+                  ## remove trailing "/" on dir and url ##
+                  db@dir <- sub("\\/$","", db@dir, perl = TRUE)
+                  db@url <- sub("\\/$","", db@url, perl = TRUE)
+                  
+                  createLocalDir(db)
+                  
+                  ## save url in the R workspace format in the main directory ##
+                  myurl <- db@url 
+                  save(myurl, file = file.path(db@dir, "url"))
+                  invisible(db)
           })
 
 setMethod("dbCreate",
           signature(db = "localDB"),
           function(db, ...) {
-              ## remove trailing "/" on dir ##
-              db@dir <- sub("\\/$","", db@dir, perl = TRUE)
+                  ## remove trailing "/" on dir ##
+                  db@dir <- sub("\\/$","", db@dir, perl = TRUE)
 
-              createLocalDir(db)
+                  createLocalDir(db)
 
-              if(!file.exists(versionFile(db)))
-                  file.create(versionFile(db))
-              invisible(db)
+                  if(!file.exists(versionFile(db)))
+                          file.create(versionFile(db))
+                  invisible(db)
           })
 
 
